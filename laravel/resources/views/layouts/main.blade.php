5a8bf5de07f4013fe29a4d6041f11e6ed972bff6
Nima Hejazi <nima@robotkudos.com>
2021-02-08 08:42:42 -0800

    Fix footer position

--- laravel/resources/views/layouts/main.blade.php
+++ laravel/resources/views/layouts/main.blade.php
@@ -9,21 +9,26 @@
       <title>@yield('title')</title>
   </head>
   <body>
-  <header class="main-header">
-      <div class="navbar" role="navigation" aria-label="main navigation">
-          <div class="navbar-brand">
-              <a class="navbar-item" href="/"><img class="logo" src="/img/logo.svg" /></a>
-              <div class="navbar-burger" role="button" aria-label="menu" aria-expanded="false" data-target="navbarMenu"><span aria-hidden="true"></span><span aria-hidden="true"></span><span aria-hidden="true"></span></div>
-          </div>
-          @yield('menu')
-      </div>
-  </header>
-  @yield('main')
-  <footer class="main-footer">
-      <div class="container">
-          <p class="copyright">&copy; Copyright {{$copyright_year}} by <a href='https://robotkudos.com' target='_blank'>Robot Kudos</a>. All rights reserved.</p>
-      </div>
-  </footer>
+
+  <div class="wrapper">
+    <div class="content-wrapper">
+        <header class="main-header">
+            <div class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="/"><img class="logo" src="/img/logo.svg" /></a>
+                    <div class="navbar-burger" role="button" aria-label="menu" aria-expanded="false" data-target="navbarMenu"><span aria-hidden="true"></span><span aria-hidden="true"></span><span aria-hidden="true"></span></div>
+                </div>
+                @yield('menu')
+            </div>
+        </header>
+        @yield('main')
+    </div>
+    <footer class="main-footer">
+        <div class="container">
+            <p class="copyright">&copy; Copyright {{$copyright_year}} by <a href='https://robotkudos.com' target='_blank'>Robot Kudos</a>. All rights reserved.</p>
+        </div>
+    </footer>
+  </div>
   </body>
   <script src="/js/bundle.js"></script>
   @yield('scripts')
