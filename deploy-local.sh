b26870419a8a33e656c2a1142d357462e96bc81f
Nima Hejazi <nima@robotkudos.com>
2021-02-26 09:37:09 -0800

    Support XDEBUG for local development

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WORKSPACE=$(pwd)
 export CONTAINER_NAME=propertyspot_laravel
-docker-compose -f docker-compose-dev.yml up -d
+docker-compose -f docker-compose-dev.yml up -d --build 
 ./composer install
 ./artisan migrate:fresh --seed
